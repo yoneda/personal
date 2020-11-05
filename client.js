2277d01fa7145cc30848ba88e4b34e467aa83103
yoneda <nsshowdream@gmail.com>
2020-11-05 16:41:50 +0900

    [fix] add semantic htmls

--- client.js
+++ client.js
@@ -7,29 +7,6 @@ import { Reset } from "styled-reset";
 import Twitter from "./Twitter";
 import Github from "./Github";
 
-const StatusGood = styled.span`
-  color: green;
-`;
-
-const StatusBad = styled.span`
-  color: red;
-`;
-
-const Box = styled.div`
-  border: 1px solid black;
-  border-radius: 5px;
-  margin: 10px;
-  padding: 10px;
-  width: 400px;
-`;
-
-const HomeBox = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 const IconBox = styled.span`
   cursor: pointer;
   transition: 0.3s;
@@ -46,106 +23,83 @@ const H2 = styled.h2`
   line-height: 2em;
 `;
 
-function Home() {
-  return (
-    <HomeBox>
-      <section>
-        <H2>Kohei Yoneda</H2>
-        <p>関西に住むフリーランスエンジニア。React を書くのが好きです。</p>
-        <p>
-          趣味は料理やお菓子作りなど。最近は
-          <a href="https://yoneda.fun">日記アプリ</a>
-          を作っています。
-        </p>
-        <IconBox>
-          <Twitter />
-        </IconBox>
-        <IconBox>
-          <Github />
-        </IconBox>
-        <p>ホーム | プロジェクト | 記事</p>
-      </section>
-    </HomeBox>
-  );
-}
-
-function About() {
-  const [name, setName] = useState("");
-  useEffect(() => {
-    const url = "https://reqres.in/api/users/1";
-    request
-      .get(url)
-      .then((res) => res.body)
-      .then((body) => {
-        setName(body.data.last_name);
-      });
-  }, []);
-  return (
-    <div>
-      <Nav />
-      <div>This is about page</div>
-      {name && <div>{name}</div>}
-    </div>
-  );
-}
-
-function Works() {
-  return (
-    <div>
-      <Nav />
-      <div>This is works page</div>
-      <p>Please check my awesome works.</p>
-    </div>
-  );
-}
-
-function Contact() {
-  return (
-    <div>
-      <Nav />
-      <div>This is contact page</div>
-      <p>Mail to me.</p>
-    </div>
-  );
-}
-
-function Nav() {
-  return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="./../about">about</Link> |{" "}
-      <Link to="./../works">works</Link> |{" "}
-      <Link to="./../contact">contant</Link> | <Link to="./../app">app</Link>
-    </nav>
-  );
-}
-
 const GlobalStyle = createGlobalStyle`
-  
   body{
     background-color: whitesmoke;
     font-family: 'M PLUS 1p', sans-serif;
     font-weight: 400;
     font-size: 1.2em;
     line-height: 1.5em;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   a:visited{
     color: crimson;
   }
+
+  section{
+    width: 600px;
+  }
 `;
+
+function Header() {
+  return (
+    <header>
+      <H2>Kohei Yoneda</H2>
+      <div>Toggle</div>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <section>
+        <p>
+          兵庫県に住むソフトウェアエンジニア。React と Swift
+          のコードをよく書いています。最近は<a href="">簡単日記</a>
+          を作っています。
+        </p>
+        <p>
+          2020年には<a href="">野菜スペシャリスト</a>
+          の資格を取得。野菜の正しい知識を世の中に広める活動を行っている。
+        </p>
+      </section>
+      <section>
+        <IconBox>
+          <Twitter />
+        </IconBox>
+        <IconBox>
+          <Github />
+        </IconBox>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p>
+        ソースコードは<a href="">こちら</a>
+      </p>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <Fragment>
+    <div id="container">
       <Reset />
       <GlobalStyle />
-      <Router>
-        <Home path="/" />
-        <About path="/about" />
-        <Works path="/works" />
-        <Contact path="/contact" />
-      </Router>
-    </Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
